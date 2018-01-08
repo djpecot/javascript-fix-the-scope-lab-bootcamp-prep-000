88586a79498ae0fb7977e55bc3e199a499e4385a
Douglas Joseph Pecot <djpecot@gmail.com>
2018-01-08 19:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return function secondaryanimal(){
-     return animal = 'cat';
+      animal = 'cat';
   }
   return animal
 }
